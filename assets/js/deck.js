af8118e8523dc645718152a27f7e965ae7b69786
Dom-Napoli <Dom-Napoli@users.noreply.github.com>
2024-02-13 20:39:53 -0800

    Again the same error. In order to create the image element, I must have a source. Reviewed several past projects and it is coded the same way as LINE: 52 in this project. Results in error.

--- assets/js/deck.js
+++ assets/js/deck.js
@@ -12,12 +12,14 @@ const deckId = parseInt(urlParams.get("deck"));
 console.log(typeof deckId);
 
 //confirm all arrays accessed
-// console.log(decks);
-// console.log(cards);
-// console.log(cardObjects);
+console.log(decks);
+console.log(cards);
+console.log(cardObjects);
 
 let container = document.querySelector(".container");
 console.log(container);
+
+// function abandoned - kept resulting in errors
 
 //create an deck_id array
 //loop cardObjects array and iterate for deck_id
@@ -26,15 +28,15 @@ let deckPageObj = [];
 for(let i = 0; i < cardObjects.length; i++){
 if(cardObjects[i].deck_id === deckId) {
 deckPageObj.push(cardObjects[i]);
-    // console.log(cardObjects[i]);
-    // console.log(cardObjects[i].image);
-    // console.log(cardObjects[i].wtmeaning);
-    // console.log(cardObjects[i].upright);
-    // console.log(cardObjects[i].reverse);
-    // console.log(cardObjects[i].copyright);
-    // console.log(cardObjects[i].horoscope);
-    // console.log(cardObjects[i].card_order);
-    // console.log(cardObjects[i].alt_name);
+    console.log(cardObjects[i]);
+    console.log(cardObjects[i].image);
+    console.log(cardObjects[i].wtmeaning);
+    console.log(cardObjects[i].upright);
+    console.log(cardObjects[i].reverse);
+    console.log(cardObjects[i].copyright);
+    console.log(cardObjects[i].horoscope);
+    console.log(cardObjects[i].card_order);
+    console.log(cardObjects[i].alt_name);
 
     let deckContainer = document.createElement("div");
     // console.log(deckContainer);
@@ -43,18 +45,12 @@ deckPageObj.push(cardObjects[i]);
     let cardImgDiv = document.createElement("div");
         cardImgDiv.setAttribute("class", "card-img-div");
         console.log(cardImgDiv); 
+
+    let cardBackImg = document.createElement("img");
+        cardBackImg.setAttribute("class", "card-back");
+        console.log(cardBackImg);
+        cardBackImg.setAttribute("src", decks[i].card_back);
+        console.log(cardBackImg);
     }
 }
-// console.log(deckPageObj);
-
-let cardBackImg = [];
-for(let i = 0; i < decks.length; i++){
-    if(decks[i].card_back === 3){
-    cardBackImg.push(decks[i]);
-
-let cardBackImage = document.createElement("img");
-    cardBackImage.setAttribute("class", "card-back-img");
-    cardBackImage.setAttribute("src", decks[i].card_back);
-    console.log(cardBackImage);  
-    }
-}
+console.log(deckPageObj);

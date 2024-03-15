3c885a3b5d79d09b31a023c7d7e3aa33fd5be742
Dom-Napoli <Dom-Napoli@users.noreply.github.com>
2024-03-14 23:04:07 -0700

    Changes made to deck.js - please read comments.

--- assets/js/deck.js
+++ assets/js/deck.js
@@ -16,49 +16,35 @@ console.log(typeof deckId);
 // console.log(cards);
 // console.log(cardObjects);
 
+//VAR assigned to deck
+let selectedDeck
+//For loop to index through decks.js array and pass the deck type to the corresponding deck page.
+for (let i = 0; i < decks.length; i++) {
+    console.log(decks[i]);
+    //CL result confirms all three decks accessed
+    if (decks[i].id === deckId) {
+        selectedDeck = decks[i];
+    }
+}
+//CL result confirms deck type assigned to specific deck page. Tested by clicking on home page.
+console.log(selectedDeck);
+
+//VAR assigned to card 
+let selectedDeckCard
+//For loop to index through cards.js to access name and suit for each card.
+for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i]);
+    //CL results all 79 cards accessed.
+    if (cards[i].id === deckId) {
+        //If resulted in reference error as not defined when I used cardId.
+        //Changed to deckId - see console.log LINE: 45
+        selectedDeckCard = cards[i];
+    }
+}
+//CL result as card id: 1
+console.log(selectedDeckCard);
+
+//VAR - create deck page container
 let container = document.querySelector(".container");
 console.log(container);
 
-//create an deck_id array
-//loop cardObjects array and iterate for deck_id
-//console.log results deck.html passed cards per specific deck_id
-let deckPageObj = [];
-for(let i = 0; i < cardObjects.length; i++){
-if(cardObjects[i].deck_id === deckId) {
-deckPageObj.push(cardObjects[i]);
-
-// console.log(cardObjects[i]);
-// console.log(cardObjects[i].image);
-// console.log(cardObjects[i].wtmeaning);
-// console.log(cardObjects[i].upright);
-// console.log(cardObjects[i].reverse);
-// console.log(cardObjects[i].copyright);
-// console.log(cardObjects[i].horoscope);
-// console.log(cardObjects[i].card_order);
-// console.log(cardObjects[i].alt_name);
-
-    //create div for card container
-    let cardContainer = document.createElement("div");
-    cardContainer.setAttribute("class", "card-container");
-    console.log(cardContainer);
-
-    //ceate div for card image
-    let picDiv = document.createElement("div");
-    picDiv.setAttribute("class", "pic-div");
-    console.log(picDiv);
-
-    let cardBack;
-    for (let i = 0; i < decks.length; i++){
-    if(decks[i].deck_id === decks.card_back){
-        cardBack = decks[i];
-    }
-    console.log(cardBack)
-}
-    
-    let imgBack = document.createElement("img");
-    imgBack.setAttribute("class", "img-back");
-    imgBack.setAttribute("src", decks[i]);
-    console.log(imgBack);
-}
-}
-console.log(deckPageObj);
